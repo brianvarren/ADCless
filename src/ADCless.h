feebaa5d39e0aa0b9523b50889d8a7cb252ee020
Brian Varren <86057806+brianvarren@users.noreply.github.com>
2025-09-02 15:05:56 -0700

    Update ADCless.h

--- src/ADCless.h
+++ src/ADCless.h
@@ -9,6 +9,8 @@
 #include "hardware/irq.h"
 
 #define NUM_ADC_INPUTS      8
+
+// Uncomment one depending on the target
 #define ADCLESS_RP2350B
 //#define ADCLESS_RP2040
 
@@ -18,10 +20,8 @@
     #define BASE_ADC_PIN 26
 #endif
 
-// DECLARATIONS only (using extern keyword)
 extern volatile uint16_t adc_results_buf[NUM_ADC_INPUTS];
 extern volatile uint16_t* adc_results_ptr[1];
 extern uint adc_samp_chan, adc_ctrl_chan;
 
-// Function declaration only
 void configureADC_DMA();
